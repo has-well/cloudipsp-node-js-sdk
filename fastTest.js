80691aa22fe5dada813afd8b9e53e6b6846b0c53
has.well <dmmatrix222@gmail.com>
2019-09-23 18:12:12 +0300

    аа

--- fastTest.js
+++ fastTest.js
@@ -1,16 +1,18 @@
-const CloudIpsp = require('./lib');
+const CloudIpsp = require('./lib')
 
 const tt = new CloudIpsp(
-    {
-        protocol: '1.0',
-        merchantId: 1396424,
-        baseUrl: 'api.fondy.eu',
-        secretKey: "test",
-        creditKey: "testcredit",
-        contentType: 'json'
-    }
-);
-let data = {
-
-};
-console.log(tt.Checkout());
+  {
+    protocol: '1.0',
+    merchantId: 1396424,
+    baseUrl: 'api.fondy.eu',
+    secretKey: 'test',
+    creditKey: 'testcredit',
+    contentType: 'form'
+  }
+)
+const data = {
+  order_desc: 'test order',
+  currency: 'USD',
+  amount: '125'
+}
+console.log(tt.Checkout(data))
